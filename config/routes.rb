3b73a63dad968cf7ea65ca01b4911deb0320fa45
imdad <imdadsemail@gmail.com>
2025-03-19 01:38:58 +0530

    first commnit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'products#index'
+
+
+  get 'products/', to: 'products#index'
+  get 'products/new', to: 'products#new', as: 'new_product'
+  get 'products/:id', to: 'products#show', as: 'product'
+  post 'products/', to: 'products#create'
+  get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
+  patch 'products/:id', to: 'products#update'
+
+end
