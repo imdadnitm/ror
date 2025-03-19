d766310eaeb0015172e9792f54f8fbb6cdde22c6
imdad <imdadsemail@gmail.com>
2025-03-20 01:36:32 +0530

    added delete functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
+
+  before_action :set_product, only: %i[show edit update destroy]
   
   def index
       @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+  
   end
 
   def new
@@ -23,13 +25,9 @@ class ProductsController < ApplicationController
 
   def edit 
     
-    @product = Product.find(params[:id])
-
   end
 
   def update
-
-    @product = Product.find(params[:id])
 
     if @product.update(product_params)
       redirect_to @product
@@ -39,10 +37,19 @@ class ProductsController < ApplicationController
 
   end
 
+  def destroy
+     @product.destroy
+     redirect_to products_path
+  end
+
 
   private
     def product_params
       params.expect(product: [ :name ])
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
     end
 
 end
